5f0f9a6ffb2b1f9215db8fa9b86c30a6a471826b
81hamidreza81 <hamiddesignertube@gmail.com>
2020-10-01 16:18:23 +0330

    upload game

---
+++ offline.js
@@ -0,0 +1,23 @@
+﻿{
+	"version": 1601582736,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/sprite3-sheet0.png",
+		"images/b1-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite4-sheet0.png",
+		"images/b2-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/dorbin-sheet0.png",
+		"images/mane-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
